8dfeca9ea368305daca6cf238b5188847874bcf6
Daniel Iván López García <lopez.garcia1319@gmail.com>
2022-06-19 04:14:31 -0600

    homeview

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,67 +1,124 @@
 <template>
-
-<div class="uk-section uk-section-secondary uk-light">
+  <div class="uk-section uk-section-secondary uk-light">
     <div class="uk-container uk-margin">
-        <h3>Bustop</h3>
-        <p class="uk-text-justify">Cansado siempre de preguntar: ¿Dónde pasa esta ruta? ¿Esta pasa por el centro? ¿Va directo?
-        No te preocupes, aquí podrás encontrar la ruta que necesitas. Bustop ha recolectado la información
-        de todas las rutas y sus recorridos a tu disposición.</p>
-        <p></p><br>
+      <h3>Bustop</h3>
+      <p class="uk-text-justify">
+        Cansado siempre de preguntar: ¿Dónde pasa esta ruta? ¿Esta pasa por el
+        centro? ¿Va directo? No te preocupes, aquí podrás encontrar la ruta que
+        necesitas. Bustop ha recolectado la información de todas las rutas y sus
+        recorridos a tu disposición.
+      </p>
+      <p></p>
+      <br />
     </div>
-    <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s" uk-grid>
-    <div class="uk-card-media-center uk-cover-container">
-        <img src="../assets/images/b2.jpg" alt="" uk-cover>
+    <div
+      class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s"
+      uk-grid
+    >
+      <div class="uk-card-media-center uk-cover-container">
+        <img src="../assets/images/b2.jpg" alt="" uk-cover />
         <canvas width="600" height="400"></canvas>
-    </div>
-    <div>
+      </div>
+      <div>
         <div class="uk-card-body uk-margin-remove">
-        
-            <h3 class="uk-card-title" id="title-section-1">Porque viajar en transporte publico ahora es más fácil</h3>
-            <p>Bustop te ayuda a que encuentres tu ruta de transporte de una manera más fácil sin tanta vuelta.</p>
+          <h3 class="uk-card-title" id="title-section-1">
+            Porque viajar en transporte publico ahora es más fácil
+          </h3>
+          <p>
+            Bustop te ayuda a que encuentres tu ruta de transporte de una manera
+            más fácil sin tanta vuelta.
+          </p>
         </div>
+      </div>
     </div>
-</div>
 
-<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s" uk-grid>
-    <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
-        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: fade; autoplay: true; autoplay-interval: 6000" uk-cover>
-
-    <ul class="uk-slideshow-items">
+    <div
+      class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s"
+      uk-grid
+    >
+      <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
+        <div
+          class="uk-position-relative uk-visible-toggle uk-light"
+          tabindex="-1"
+          uk-slideshow="animation: fade; autoplay: true; autoplay-interval: 6000"
+          uk-cover
+        >
+          <ul class="uk-slideshow-items">
             <li>
-            <img src="../assets/images/place1.jpg" alt="" >
-        </li>
-<li>
-            <img src="../assets/images/place2.jpg" alt="" >
-        </li>
-        <li>
-            <img src="../assets/images/place5.jpg" alt="" >
-        </li>
-        <li>
-            <img src="../assets/images/place3.jpg" alt="" >
-        </li>
-    </ul>
+              <img src="../assets/images/place1.jpg" alt="" />
+            </li>
+            <li>
+              <img src="../assets/images/place2.jpg" alt="" />
+            </li>
+            <li>
+              <img src="../assets/images/place5.jpg" alt="" />
+            </li>
+            <li>
+              <img src="../assets/images/place3.jpg" alt="" />
+            </li>
+          </ul>
 
-    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
-<canvas width="600" height="390"></canvas>
-</div>
-        
-    </div>
-    <div>
+          <a
+            class="uk-position-center-left uk-position-small uk-hidden-hover"
+            href="#"
+            uk-slidenav-previous
+            uk-slideshow-item="previous"
+          ></a>
+          <a
+            class="uk-position-center-right uk-position-small uk-hidden-hover"
+            href="#"
+            uk-slidenav-next
+            uk-slideshow-item="next"
+          ></a>
+          <canvas width="600" height="390"></canvas>
+        </div>
+      </div>
+      <div>
         <div class="uk-card-body">
-            <h3 class="uk-card-title" id="title-section-1">Viaja por todo El Salvador</h3>
-            <p>Puedes viajar a los lugares más emblematicos de El Salvador usando transporte público, con la ayuda de Bustop lo lograrás.</p>
+          <h3 class="uk-card-title" id="title-section-1">
+            Viaja por todo El Salvador
+          </h3>
+          <p>
+            Puedes viajar a los lugares más emblematicos de El Salvador usando
+            transporte público, con la ayuda de Bustop lo lograrás.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="uk-child-width-1-1" uk-grid>
+      <span class="uk-text-lead uk-text-center">BUSTOP TE AYUDA A:</span>
+    </div>
+    <div class="uk-child-width-1-2@m uk-padding" uk-grid>
+    <div>
+        <div class="uk-card uk-card-default" id="section-card">
+            <div class="uk-card-media-top">
+                <img src="../assets/images/iconos/bus.png" id="icons-section"  alt="">
+            </div>
+            <div class="uk-card-body uk-text-center uk-text-justify">
+                <p>Encontrar la ruta que deseas con el amplio catalogo de rutas que Bustop tiene a tu disposición.</p>
+                
+            </div>
         </div>
     </div>
-</div>
-</div>
 
+        <div>
+        <div class="uk-card uk-card-default" id="section-card">
+            <div class="uk-card-media-top" >
+                <img src="../assets/images/iconos/maps.png"  alt="" id="icons-section">
+            </div>
+            <div class="uk-card-body uk-text-center uk-text-justify">
+                <p>Orientarte a través de los mapas de cada ruta para que puedas llegar a tu destino deseado.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MenuBar',
-
-}
+  name: "MenuBar",
+};
 </script>
 
