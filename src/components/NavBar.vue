0eb2862b7226b20bb527fcada1972f8f00dd2d80
Daniel Iván López García <lopez.garcia1319@gmail.com>
2022-06-23 15:22:51 -0600

    login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,36 +1,66 @@
 <template>
-    <div
-    uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky" class="stickyUI" >
+  <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky" class="stickyUI">
     <nav class="uk-navbar uk-navbar-container uk-margin">
       <div class="nav-overlay uk-navbar-left">
         <ul class="uk-navbar-nav">
+          <li v-if="userAuth">
+            <a href="">logeado</a>
+          </li>
           <li>
             <router-link to="/">Inicio</router-link>
           </li>
           <li>
             <router-link to="/buses">Ruta de Buses</router-link>
           </li>
-          <li><a href="#formulario_login" uk-toggle>Iniciar Sesión</a></li>
+          <li v-if="!userAuth"><a href="#formulario_login" uk-toggle>Iniciar Sesión</a></li>
         </ul>
       </div>
       <div class="nav-overlay uk-navbar-right">
-        <a class="uk-navbar-toggle" uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+        <a class="uk-navbar-toggle" uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade"
+          href="#"></a>
       </div>
-      
-    <div class="nav-overlay uk-navbar-left uk-flex-1" hidden>
+
+      <div class="nav-overlay uk-navbar-left uk-flex-1" hidden>
 
         <div class="uk-navbar-item uk-width-expand">
-            <form class="uk-search uk-search-navbar uk-width-1-1">
-                <input class="uk-search-input" type="search" placeholder="Buscar Ruta..." autofocus>
-            </form>
+          <form class="uk-search uk-search-navbar uk-width-1-1">
+            <input class="uk-search-input" type="search" placeholder="Buscar Ruta..." autofocus>
+          </form>
         </div>
 
-        <a class="uk-navbar-toggle" uk-close uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+        <a class="uk-navbar-toggle" uk-close uk-toggle="target: .nav-overlay; animation: uk-animation-fade"
+          href="#"></a>
 
-    </div>
+      </div>
     </nav>
   </div>
 
-   
+
   <router-view></router-view>
 </template>
+<script>
+
+export default {
+
+  data() {
+    return {
+      userAuth: false
+    }
+  },
+  methods: {
+    logincheck() {
+      const check = localStorage.getItem('user')
+      if (check !== null && check !== '') {
+        this.userAuth = true
+      } else {
+        this.userAuth = false
+      }
+      return this.userAuth
+    }
+  },
+   created:function(){
+      this.logincheck();
+    }
+
+}
+</script>
--- src/modals/AdminloginForm.vue
+++ src/modals/AdminloginForm.vue
@@ -2,48 +2,82 @@
     <form class="uk-grid-small" @submit.prevent="onSubmit" uk-grid>
         <div class="uk-width-1-1">
             <label for="txt-nickname" class="uk-label-form">Nombre de usuario</label>
-            <input class="uk-input" id="txt-nickname" type="text" placeholder="example123" v-model="nickname">
+            <input class="uk-input" id="txt-nickname" type="text" placeholder="example123" v-model="logDetail.nickname">
         </div>
         <div class="uk-width-1-1">
             <label for="txt-pass" class="uk-label-form">Contraseña</label>
-            <input class="uk-input" type="password" placeholder="******" v-model="password">
+            <input class="uk-input" type="password" placeholder="******" v-model="logDetail.password">
         </div>
         <div class="uk-width-expand">
             <button class="uk-button uk-button-secondary " type="submit">Iniciar Sesión</button>
         </div>
     </form>
+<div class="uk-alert-success" v-if="successMessage" uk-alert>
+    <a class="uk-alert-close" uk-close="duration:500"></a>
+    <p> {{  successMessage }} </p>
+</div>
+<div class="uk-alert-danger" v-if="errorMessage" uk-alert="duration: 500">
+    <a class="uk-alert-close" uk-close></a>
+    <p> {{  errorMessage }} </p>
+</div>
+<div v-if="userAuth">
+hola logueado
+</div>
 </template>
 <script>
-//import axios from 'axios'
+import axios from 'axios'
+//import { response } from 'express'
     export default {
         data(){
             return{
-                nickname: "",
-                password:""
+
+                logDetail:{nickname: "", password:""},
+                errorMessage:"",
+                successMessage:"",
+                userAuth: false
             }
         },
         methods:{
-            onSubmit(){
-                let datos = {
-                nickname: this.nickname,
-                password:this.password
-                };
+        toFormData(obj){
+                var form_data = new FormData();
 
-                fetch('http://localhost/API/maps/c_login.php',{
-                    method:'POST',
-                    headers:{'Content-Type':'application/json'},
-                    body:JSON.stringify(datos)
-                }).then( res =>{
-                    console.log(res)
-                    
+                for(var key in obj){
+                    form_data.append(key,obj[key])
+                }
+                return form_data;
+            },
+
+            getDataLogin(){
+                var logForm = this.toFormData(this.logDetail)
+                axios.post('http://localhost/API/maps/c_login.php', logForm).then(response => {
+                    if(response.data.error){
+                        this.errorMessage = response.data.message
+                    }else{
+                        this.successMessage= response.data.message
+                        this.logDetail = {nickname:"",password:""}
+
+                        let data =[{tokenid:response.data.token, id: response.data.id, name: response.data.username }]
+                        localStorage.setItem('user', JSON.stringify(data))
+                        
+                        
+                        console.log(response)
+                    }
                 }).catch(error => {
-                    
-                    console.log(error)
-                });
-                
-                
 
-            }
-        }
+                    console.log(error)
+
+                })
+            },
+
+           async onSubmit(){
+            await this.getDataLogin()
+            },
+
+
+        },
+       
+
+
+
     }
 </script>
